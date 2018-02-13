0acebf30b001b04e00f08643820500e63bbe0f35
Andy Watkins <awatkins@ukcloud.com>
2018-02-13 21:18:28 +0000

    Mechanism and clockfaces separated into objects.
    Reorganised code into a library,
    added a Readme.
    Lots of tidying up

--- clock.js
+++ clock.js
@@ -1,30 +1,4 @@
-<html>
-    <head>
-        <script>
-/*
-    Draw a (digital) clock with a start time specified by the first 
-    item in 'timetable'
 
-    time offset is a Global.
-
-    TODO:
-        [Y] at the start, build an array of "slow-down" and "speed-up" times,
-
-        [Y] clockspeed of 600 only works if we tick every second.
-            we really need tick interval to be less (e.g. 15) and for
-            the clockspeed to be relative to it (e.g. 15/1000)*600
-        [Y] Change speed at boundaries.
-
-        [Y] put all the clock mechanism code in an object.
-
-        [Y] move "digital clock" to a clockFace object.
-
-        [] make sure the timetable is chronological
-
-        [] second hand needs attention in high-speed mode (should
-           stay where it was when speed increased)
-
-*/
 
 /*
     Times in the timetable must be 'hh:mm:ss'
@@ -33,21 +7,6 @@ var timetable = [
     ['09:58:00', '09:58:15'], // '10:03:45'
     ['10:30:00', '10:33:00']
 ];
-
-
-// Start times may need to be adjusted to cater for accelerated clock time.
-// e.g. start 15s before you need to.
-// TODO: work out what to say about the end-end time.
-// e.g. add a 4-hour run-off block? or will the clock stop?
-// The time when the clock slows down may be a while after the stated time
-// because the clock needs to slow down. (about 20-30s with current settings)
-
-timetable = [
-    ['09:00:00', '09:00:10'],
-    ['09:59:55', '10:00:30']
-];
-
-
 
 
 // Definition of ClockMechanism object
@@ -99,15 +58,16 @@ function ClockMechanism() {
     */
     this.getTime = function() {
         var now = new Date(new Date().getTime() - this.timeOffset * 1000);
+        var isAccelerated = false;
         this.hour = now.getHours();
         this.minute = now.getMinutes();
         this.second = now.getSeconds();
 
         // pre-tick operations: before drawing the clock at each tick
         if (this.clockSpeed > 1) {
-            this.second = 0;
+            isAccelerated = true;
         }
-        return [this.hour, this.minute, this.second];
+        return [this.hour, this.minute, this.second, now.getMilliseconds(), isAccelerated];
     }
 
     /*
@@ -191,9 +151,7 @@ function DigitalClockFace(elementId, mechanism) {
         var hms = this.mech.getTime();
         var h = hms[0], m=hms[1], s=hms[2];
 
-        document.getElementById(this.elementId).innerHTML = this._padStart(h) +
-                                                        ":" + this._padStart(m) +
-                                                        ":" + this._padStart(s);
+        document.getElementById(this.elementId).innerHTML = this._padStart(h) + ":" + this._padStart(m) + ":" + this._padStart(s);
         this.mech.tick();
     }
 
@@ -205,19 +163,131 @@ function DigitalClockFace(elementId, mechanism) {
     }
 }
 
-        </script>
-    </head>
+/*
+    Analogue Clock face
+    Clock appearance from
+    https://www.w3schools.com/graphics/canvas_clock_start.asp
+*/
+function AnalogueClockFace(canvasElementId, mechanism) {
+    this.elementId = canvasElementId;
+    this.mech = mechanism;
+    this.smoothSeconds = true;
 
+    this.setup = function() {
+        var canvas = document.getElementById("canvas");
+        this.ctx = canvas.getContext("2d");
+        radius = canvas.height / 2;
+        this.ctx.translate(radius, radius); // remap 0,0 to the centre of the canvas.
+        radius = radius * 0.90;
+        this.radius = radius;
 
-    <body>
-        <p id="digitalclock"></p>
+        this.hourHandLength = radius*0.5;
+        this.hourHandWidth = radius*0.07;
+        this.minuteHandLength = radius*0.8;
+        this.minuteHandWidth = radius*0.07;
+        this.secondHandLength = radius*0.9;
+        this.secondHandWidth = radius*0.02;
 
-        <script>
-            var mech = new ClockMechanism();
-            var face = new DigitalClockFace('digitalclock', mech);
-            mech.setup(timetable);
-            face.draw();
-            setInterval(function(){face.draw();}, mech.tickInterval);
-        </script>
-    </body>
-</html>
+        // used to keep second hand still when accelerated.
+        this.lastSeconds = 0;
+    }
+
+    /*
+        Draw the clock face at this tick.
+        calls mech.getTime() to work out the time, then mech.tick()
+        to advance the time.
+        This method is expected to be called every animation frame.
+    */
+    this.draw = function() {
+        var hms = this.mech.getTime();
+        var h = hms[0], m=hms[1], s=hms[2];
+
+        if (this.smoothSeconds) {
+            var ms = hms[3];
+        }else{
+            var ms = 0;
+        }
+        if (hms[4]) {  // accelerated mode
+            s = this.lastSeconds;
+            ms = 0;
+        }else{
+            this.lastSeconds = s + 1;
+        }
+
+        this._drawFaceBackplate();
+        this._drawNumbers();
+        this._drawTime(h, m, s + (ms/1000));
+    }
+
+    /*
+        Draw the backplate on which the clock face will 
+        be drawn; background circle etc.
+    */
+    this._drawFaceBackplate = function() {
+        var ctx = this.ctx;
+
+        ctx.beginPath();
+        ctx.arc(0, 0, this.radius, 0, 2*Math.PI);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+
+        var grad = ctx.createRadialGradient(
+            0, 0, this.radius * 0.95, 0, 0, this.radius * 1.05);
+        grad.addColorStop(0, '#333');
+        grad.addColorStop(0.5, 'white');
+        grad.addColorStop(1, '#333');
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = this.radius * 0.1;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(0, 0, this.radius * 0.1, 0, 2 * Math.PI);
+        ctx.fillStyle = '#333';
+        ctx.fill();
+    }
+
+    /*
+        Draw the numbers around the clock face
+    */
+    this._drawNumbers = function() {
+        var ang;
+        var num;
+        var ctx = this.ctx;
+        ctx.font = this.radius * 0.15 + "px arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        for(num = 1; num <= 12; num++){
+            ang = num * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -this.radius * 0.85);
+            ctx.rotate(-ang);
+            ctx.fillText(num.toString(), 0, 0);
+            ctx.rotate(ang);
+            ctx.translate(0, this.radius * 0.85);
+            ctx.rotate(-ang);
+        }
+    }
+    this._drawTime = function(hour, minute, second) {
+        hour=hour%12;
+        hour=(hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+        minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+        second=(second*Math.PI/30);
+        this._drawHand(hour, this.hourHandLength, this.hourHandWidth);
+        this._drawHand(minute, this.minuteHandLength, this.minuteHandWidth);
+        this._drawHand(second, this.secondHandLength, this.secondHandWidth);
+    }
+
+    this._drawHand = function(pos, length, width) {
+        var ctx = this.ctx;
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
+    }
+
+}
+
